d366fed7c4eb96d273a0590f81758358e38c7476
vishal <vishal.t.15khade@gmail.com>
2022-07-02 21:59:52 +0530

    First round of commit for FirstProject

---
+++ src/test/java/tests/TestClass1.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestClass1{
+	
+	
+	@Test
+	public void test1()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test1 passed");
+	}
+	
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test2 passed");
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/tests/TestClass2.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+	@Test
+	public void test3()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test3 passed");
+		
+	}
+	
+	
+	@Test
+	public void test4()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test4 passed");
+	}
+}
