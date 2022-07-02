3d262b7470f67010bc2ef8367593c6cf717b5333
vishal15khade <102546554+vishal15khade@users.noreply.github.com>
2022-07-02 22:48:08 +0530

    Create NewClass.java
    This is added by  somebody else

---
+++ src/test/java/tests/NewClass.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class NewClass{
+	
+	
+	@Test
+	public void test5()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test5 passed");
+	}
+	
+	
+	@Test
+	public void test6()
+	{
+		Assert.assertTrue(true);
+		System.out.println("test6 passed");
+		
+	}
+	
+	
+
+}
